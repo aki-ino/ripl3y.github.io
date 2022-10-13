e4c1d98acb111bd47d133894d16e774f0270c5fa
Ripley <72573511+ripl3yy@users.noreply.github.com>
2022-10-13 12:32:37 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    const loader = document.querySelector('.loader');
+    loader.classList += ' loaded';
+
+    setTimeout(() => {
+      loader.classList += ' hidden';
+    }, 2325);
+  }, 1000);
+});
